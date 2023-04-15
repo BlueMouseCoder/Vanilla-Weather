f1c713e7f1b061d68936346c2ff6b781e4c51470
BlueMouseCoder <130068107+BlueMouseCoder@users.noreply.github.com>
2023-04-15 09:52:13 -0400

    Added axios to html, added API to js
    Still need to confirm api key works

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
+function displayTemperature(response) {
+  console.log(response.data.main.temp);
+}
 
+let apiKey = "46666120a55ab9218384642a2eff12cc";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New-York&appid=${apiKey}&units=imperial`;
+
+console.log(apiUrl);
+
+axios.get(apiUrl).then(displayTemperature);
